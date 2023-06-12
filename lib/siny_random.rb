8299261e0cfacdcd4dc7feca0ad843004ec802ab
nautilus@octopus <nautilus@trovaprezzi.it>
2023-06-12 17:32:31 +0200

    Prove varie

---
+++ lib/siny_random.rb
@@ -0,0 +1,5 @@
+class SinyRandom
+  def get
+    "MQ: #{Random.rand.to_s}"
+  end
+end
--- siny.rb
+++ siny.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
+require_relative './lib/siny_random'
 
 class Siny < Sinatra::Base
   get '/' do
     time = DateTime.now
     File.write('./log/siny.log', time.to_s + "\n", mode: 'a')
-    "Hello! It's: #{time}"
+    "Hello! It's: #{time}... #{SinyRandom.new.get}"
   end
 
   get '/log' do
