2ac5b23b30758ab20fd1994238bccebcc9c1537b
nautilus@octopus <nautilus@trovaprezzi.it>
2023-06-14 17:35:05 +0200

    Fix

--- spec/siny_spec.rb
+++ spec/siny_spec.rb
@@ -1,7 +1,13 @@
+require_relative '../lib/siny_random'
+
 describe 'siny' do
 
   it 'should be pass' do
     expect(true).to be_truthy
+  end
+
+  it 'should be pass siny random' do
+    expect(SinyRandom.new.get).to include('MQ')
   end
 
 end
